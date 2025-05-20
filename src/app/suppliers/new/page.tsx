e7ea026f4788818d6fbe4bfd3715c57abe15de33
Sneyderbg <sneyder.buitrago@udea.edu.co>
2025-05-19 22:46:21 -0500

    use a new way for creating forms

--- src/app/suppliers/new/page.tsx
+++ src/app/suppliers/new/page.tsx
@@ -1,140 +1,155 @@
 "use client";
 
 import { Info } from "@/components/common/Info";
-import { BasicForm, FieldConfigs } from "@/components/suppliers/BasicForm";
+import { FieldConfigs, PartialForm } from "@/components/common/PartialForm";
 import { NewProduct } from "@/components/suppliers/NewProduct";
 import { ProductCard } from "@/components/suppliers/ProductCard";
 import { StepScreen } from "@/components/suppliers/StepScreen";
 import { Summary } from "@/components/suppliers/Summary";
 import { Button } from "@/components/ui/button";
-import { useRef, useState } from "react";
-import { FieldValues } from "react-hook-form";
+import { createDefaultValues, createSchema } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
 import { ClassNameValue } from "tailwind-merge";
 
 export default function NewSupplier() {
   const [step, setStep] = useState(1);
   const [showProductForm, setShowProductForm] = useState(false);
-  const supplierData = useRef<Record<string, unknown>>({});
+  const allFields = {
+    ...generalInfoFields,
+    ...contactInfo,
+    ...paymentConditions,
+  };
+
+  const form = useForm({
+    defaultValues: {
+      ...createDefaultValues(allFields),
+      products: [],
+    },
+    resolver: zodResolver(createSchema(allFields)),
+    mode: "onChange",
+  });
+
+  const { append } = useFieldArray({
+    control: form.control,
+    name: "products",
+  });
 
   const nextStep = () => setStep((s) => s + 1);
-
-  const handleStepSubmit = (data: FieldValues) => {
-    Object.entries(data).forEach(([k, v]) => {
-      supplierData.current[k] = v;
-    });
-    nextStep();
-  };
 
   const innerDivCn: ClassNameValue =
     "grid grid-cols-2 justify-around items-center px-4 gap-16 w-full";
 
   return (
-    <div className="flex flex-col w-full h-full p-12 overflow-x-hidden">
-      <StepScreen show={step == 1}>
-        <Info
-          title="Agregar proveedor"
-          desc="Por favor, completa la información necesaria para registrar un nuevo proveedor."
-        />
-
-        <div className={innerDivCn}>
+    <FormProvider {...form}>
+      <div className="flex flex-col w-full h-full p-12 overflow-x-hidden">
+        <StepScreen show={step == 1}>
           <Info
-            title="Paso 1 - Datos Generales"
-            desc="Ingrese los datos generales del proveedor."
+            title="Agregar proveedor"
+            desc="Por favor, completa la información necesaria para registrar un nuevo proveedor."
           />
-          <BasicForm onSubmit={handleStepSubmit} fields={generalInfoFields} />
-        </div>
-      </StepScreen>
 
-      <StepScreen show={step == 2}>
-        <Info
-          title="Paso 2 – Productos Asociados"
-          desc="¿Qué productos ofrece este proveedor?"
-        >
-          <Button variant={"outline"}>Eliminar Producto</Button>
-
-          <Button onClick={() => setShowProductForm(true)}>
-            Agregar Producto
-          </Button>
-        </Info>
-        <div className={innerDivCn}>
-          <Info title="Lista de Productos">
-            <Button onClick={() => nextStep()}>Siguiente</Button>
-            <Button>Guardar Productos</Button>
-          </Info>
-          <div className="flex flex-wrap gap-4">
-            <ProductCard
-              info={{
-                name: "Croquetas Premium DogFood",
-                brand: "DogFood Brand",
-                price: 25,
-                inStock: true,
-              }}
-            />
-            <ProductCard
-              info={{
-                name: "Plush Cat Toy",
-                brand: "CatToys Inc.",
-                price: 15,
-                inStock: true,
-              }}
-            />
-          </div>
-        </div>
-        <NewProduct
-          open={showProductForm}
-          onOpenChange={setShowProductForm}
-          onSubmit={(data) => {
-            if (supplierData.current.products instanceof Array) {
-              supplierData.current.products.push(data);
-            } else {
-              supplierData.current.products = new Array(data);
+          <PartialForm
+            leftInfo={
+              <Info
+                title="Paso 1 - Datos Generales"
+                desc="Ingrese los datos generales del proveedor."
+              />
             }
-            setShowProductForm(false);
-          }}
-          onCancel={() => setShowProductForm(false)}
-        />
-      </StepScreen>
+            onAction={nextStep}
+            fields={generalInfoFields}
+          />
+        </StepScreen>
 
-      <StepScreen show={step == 3}>
-        <Info
-          title="Paso 3 – Información de Contacto"
-          desc="Proporcione la información de contacto del proveedor."
-        />
-        <div className={innerDivCn}>
-          <Info title="Contacto" />
-          <BasicForm onSubmit={handleStepSubmit} fields={contactInfo} />
-        </div>
-      </StepScreen>
+        <StepScreen show={step == 2}>
+          <Info
+            title="Paso 2 – Productos Asociados"
+            desc="¿Qué productos ofrece este proveedor?"
+          >
+            <Button variant={"outline"}>Eliminar Producto</Button>
+            <Button onClick={() => setShowProductForm(true)}>
+              Agregar Producto
+            </Button>
+          </Info>
+          <div className={innerDivCn}>
+            <Info title="Lista de Productos">
+              <Button onClick={() => nextStep()}>Siguiente</Button>
+              <Button>Guardar Productos</Button>
+            </Info>
+            <div className="flex flex-wrap gap-4">
+              <ProductCard
+                info={{
+                  name: "Croquetas Premium DogFood",
+                  brand: "DogFood Brand",
+                  price: 25,
+                  inStock: true,
+                }}
+              />
+              <ProductCard
+                info={{
+                  name: "Plush Cat Toy",
+                  brand: "CatToys Inc.",
+                  price: 15,
+                  inStock: true,
+                }}
+              />
+            </div>
+          </div>
+          <NewProduct
+            open={showProductForm}
+            onOpenChange={setShowProductForm}
+            onSubmit={(data) => {
+              append(data);
+              setShowProductForm(false);
+            }}
+            onCancel={() => setShowProductForm(false)}
+          />
+        </StepScreen>
 
-      <StepScreen show={step == 4}>
-        <Info
-          title="Paso 4 – Condiciones de Pago"
-          desc="Defina las condiciones de pago con el proveedor."
-        />
-        <div className={innerDivCn}>
-          <Info title="Condiciones de Pago" />
-          <BasicForm onSubmit={handleStepSubmit} fields={paymentConditions} />
-        </div>
-      </StepScreen>
+        <StepScreen show={step == 3}>
+          <Info
+            title="Paso 3 – Información de Contacto"
+            desc="Proporcione la información de contacto del proveedor."
+          />
+          <PartialForm
+            leftInfo={<Info title="Contacto" />}
+            onAction={nextStep}
+            fields={contactInfo}
+          />
+        </StepScreen>
 
-      <StepScreen show={step == 5}>
-        <Info
-          title="Resumen de Proveedor"
-          desc="Revise toda la información ingresada antes de finalizar."
-        >
-          <Button variant={"outline"} onClick={() => setStep(1)}>
-            Volver a Revisar
-          </Button>
-          <Button onClick={() => console.log(supplierData.current)}>
-            Finalizar
-          </Button>
-        </Info>
-        <div className={innerDivCn}>
-          <Info title="Revisión Final" />
-          <Summary info={supplierData.current as Record<string, string>} />
-        </div>
-      </StepScreen>
-    </div>
+        <StepScreen show={step == 4}>
+          <Info
+            title="Paso 4 – Condiciones de Pago"
+            desc="Defina las condiciones de pago con el proveedor."
+          />
+          <PartialForm
+            leftInfo={<Info title="Condiciones de Pago" />}
+            onAction={nextStep}
+            fields={paymentConditions}
+          />
+        </StepScreen>
+
+        <StepScreen show={step == 5}>
+          <Info
+            title="Resumen de Proveedor"
+            desc="Revise toda la información ingresada antes de finalizar."
+          >
+            <Button variant={"outline"} onClick={() => setStep(1)}>
+              Volver a Revisar
+            </Button>
+            <Button onClick={() => console.log(form.getValues())}>
+              Finalizar
+            </Button>
+          </Info>
+          <div className={innerDivCn}>
+            <Info title="Revisión Final" />
+            <Summary info={form.getValues()} />
+          </div>
+        </StepScreen>
+      </div>
+    </FormProvider>
   );
 }
 

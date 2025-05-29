3698afacd7efb9552286124b23cef0aac0ad319d
Sneyderbg <sneyder.buitrago@udea.edu.co>
2025-05-29 17:33:52 -0500

    make layout responsive

--- src/components/atoms/navTitle/index.tsx
+++ src/components/atoms/navTitle/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 interface Indexprops {
   title?: string;
   link: string;
 }
 
-const Index = ({title = 'Title', link = '/'}: Indexprops) => {
+const Index = ({ title = "Title", link = "/" }: Indexprops) => {
   return (
     <Link href={link}>
-    <div className='h-20 w-[150] flex justify-center items-center cursor-pointer'><h1 className='text-sm text-black font-normal text-[16px]'>{title}</h1></div>
+      <div className="group h-20 w-[150] flex justify-center items-center cursor-pointer">
+        <h1 className="text-sm text-black font-normal text-[16px] transition-all group-hover:scale-105 group-hover:font-bold">
+          {title}
+        </h1>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
 export default Index;
+
